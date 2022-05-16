1f5d3184a3ef14f0bb746a404627a1067d799725
renanlimaleite <renan.lima.softwares@gmail.com>
2022-05-16 07:10:42 -0300

    add global style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/globa-styles';
 import { Home } from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>,
 );
---
+++ frontend/src/templates/Home/styles.js
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.div`
+  ${() => css`
+    background: 'blue'
+  `}
+`;
