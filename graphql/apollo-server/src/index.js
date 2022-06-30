3936fe9e0e52eecd624897465408daf7bcc0ab34
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-30 00:28:28 -0300

    add: init apollo server

--- graphql/apollo-server/src/index.js
+++ graphql/apollo-server/src/index.js
@@ -1,3 +1,13 @@
-import { Hello } from './mod';
+import { ApolloServer, gql } from 'apollo-server';
 
-console.log(Hello());
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `
+});
+
+server.listen(3333).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
